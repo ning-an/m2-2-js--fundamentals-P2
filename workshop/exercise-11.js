8ed9d4b8e8cc5b9e12a249e4a58609c68dab38e8
Ning An <ning.an@outlook.com>
2020-06-16 14:36:33 -0400

    Exercise11-solution

--- workshop/exercise-11.js
+++ workshop/exercise-11.js
@@ -2,6 +2,14 @@
 // Write a function that accepts an array of grades (numerical values) and returns the average.
 // The returned average should also be rounded to the nearest unit. (no decimals, please!)
 
-function calculateAverage(grades) {}
+function calculateAverage(grades) {
+    let total = 0;
+    let count = 0;
+    grades.forEach(function(num) {
+        total += num;
+        count++;
+    })
+    return Math.round(total / count);
+}
 
 console.log(calculateAverage([76, 60, 83, 100, 78]));
