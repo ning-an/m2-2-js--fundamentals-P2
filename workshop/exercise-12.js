7f6a13ee04ae261f94a20ab6fb7aae1edaea8646
Ning An <ning.an@outlook.com>
2020-06-16 14:46:08 -0400

    Exercise12-solution

--- workshop/exercise-12.js
+++ workshop/exercise-12.js
@@ -9,7 +9,30 @@
 // <100 is A
 
 // Write function here
+function calculateAverage(grades) {
+    let total = 0;
+    let count = 0;
+    grades.forEach(function(num) {
+        total += num;
+        count++;
+    })
+    return Math.round(total / count);
+}
 
+const gpa = function(grade) {
+    if (grade < 60) {
+        return 'F';
+    } else if (grade < 70) {
+        return 'D';
+    } else if (grade < 80) {
+        return 'C';
+    } else if (grade < 90) {
+        return 'B';
+    } else {
+        return 'A';
+    }
+}
 // Call the function with [48, 95, 65, 48, 59, 78, 72, 65]
-
+let lst = [48, 95, 65, 48, 59, 78, 72, 65];
+console.log(gpa(calculateAverage(lst)));
 // Try with other values as well
