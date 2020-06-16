ea23de4466816e6614ce3b8940abe81b686ff174
Ning An <ning.an@outlook.com>
2020-06-16 13:28:52 -0400

    Exercise2-solution

--- workshop/exercise-2.js
+++ workshop/exercise-2.js
@@ -3,7 +3,13 @@
 // the greater one.
 
 // Define the function
-function max(num1, num2) {}
+function max(num1, num2) {
+    if (num1 >= num2) {
+        return num1;
+    } else {
+        return num2;
+    }
+}
 
 // Call the function
 max(12, 43);
